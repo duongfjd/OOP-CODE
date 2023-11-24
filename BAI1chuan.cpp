9eca62af898a692f97ea355f26a117c3a26e2049
duongfjd <121930616+duongfjd@users.noreply.github.com>
2023-11-24 21:34:26 +0700

    POST3 - Ai cộng nhanh nhất
    Cho 2 số nguyên A và B . Hãy tính A + B .

---
+++ BAI1chuan.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class hehe 
+{
+
+	public:
+		hehe(float a, float b){
+		} 
+		float tong(float a, float b){
+			return a+b; 
+		} 
+
+	 
+};
+int main(){
+	float a,b;
+	hehe hehe(a,b) ;
+	cin>>a>>b;
+	cout<< hehe.tong(a,b); 
+} 
